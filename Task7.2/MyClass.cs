e8b2b82b22449428455f5dfe6db667bf0f35c9a4
Dina Nevhod <Dina_Nevhod@epam.com>
2023-04-12 13:53:16 +0200

    Add project files.

---
+++ Task7.2/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    public class MyClass
+    {
+        [Obsolete("Old method")]
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        [Obsolete("Old method",true)]
+        public void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+}
---
+++ Task7.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.Method1();
+            //myClass.Method2();
+        }
+    }
+}
